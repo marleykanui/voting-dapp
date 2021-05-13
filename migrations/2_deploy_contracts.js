3a86359196c16ef7d9e438b3c3562ba7a6c07986
Kanui <marley.kanui@gmail.com>
2021-05-12 23:22:20 -0500

    voting-dapp  - global commit 03 - set up election contract, compiled, migrated

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+let Election = artifacts.require('./Election.sol');
+
+module.exports = function (deployer) {
+  deployer.deploy(Election);
+};
+ 
--- truffle-config.js
+++ truffle-config.js
@@ -3,12 +3,17 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '*', // Match any network id
     },
     develop: {
-      port: 8545
-    }
-  }
+      port: 9545,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '^0.8.4', // A version or constraint - Ex. "^0.5.0"
+    },
+  },
 };
